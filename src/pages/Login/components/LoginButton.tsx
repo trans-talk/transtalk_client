3979aa3269d912f0cf12f25d76b39edcfbdb17f7
holdn2 <yoocy01@gmail.com>
2025-11-10 11:38:04 +0900

    style(LoginButton): update shadow for login button

--- src/pages/Login/components/LoginButton.tsx
+++ src/pages/Login/components/LoginButton.tsx
@@ -14,7 +14,7 @@ export default function LoginButton() {
   return (
     <button
       type='button'
-      className='flex w-[80%] max-w-[30rem] flex-row items-center justify-center gap-[1rem] rounded-[1rem] bg-white py-[1.2rem] shadow-[4px_4px_4px_0_rgba(0,0,0,0.25),0_0_4px_0_rgba(0,0,0,0.25)]'
+      className='flex w-[80%] max-w-[30rem] flex-row items-center justify-center gap-[1rem] rounded-[1rem] bg-white py-[1.2rem] shadow-[0_0_20px_0_rgba(0,0,0,0.15)]'
       onClick={handleLogin}
     >
       <img className='h-[2rem] w-[2rem]' src={GoogleLogo} alt='Google Logo' />
